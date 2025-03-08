02bae85a6a93a07cffa7f4227bd51084deb158b6
kaducodes <kadu.codes@gmail.com>
2025-03-08 14:15:08 -0300

    08/03/2025 - 2
    conclusão do módulo de dicionários com um exercício bem completo de leitura e análise de dados.

--- Scripts - Python/Scripts - Python Mundo 3/desafio 094.py
+++ Scripts - Python/Scripts - Python Mundo 3/desafio 094.py
@@ -15,35 +15,53 @@
 #- Lista das pessoas que estão acima da média: 
 
 lista = []
-fem = []
 acima = []
 cont = 0
-idade = 0
+soma_idade = 0
 media = 0
 
 while True:
     cont += 1
     perfil = {}
+
     perfil['nome'] = str(input("Nome: "))
-    perfil['sexo'] = str(input("Sexo [M/F]: ")).upper().strip()
-    if perfil['sexo'] == "F":
-        fem.append(perfil['nome'])
+
+    while True:
+        perfil['sexo'] = str(input("Sexo [M/F]: ")).upper().strip()
+        if perfil['sexo'] in 'MF':
+            break
+        print("ERRO! Por favor, digite apenas M ou F")
+
     perfil['idade'] = int(input("Idade: "))
-    idade += perfil['idade']
+    soma_idade += perfil['idade']
     lista.append(perfil.copy())
 
-    resp = str(input("Quer continuar? [S/N]: ")).upper().strip()
+    while True:
+        resp = str(input("Quer continuar? [S/N]: ")).upper().strip()
+        if resp in 'SN':
+            break
+        print("ERRO! Por favor, digite apenas S ou N")
     if resp == "N":
         break
 
-media = idade / cont
-for pessoa in lista:
-    if pessoa['idade'] > media:
-        acima.append(pessoa['nome'])
+media = soma_idade / cont
+for p in lista:
+    if p['idade'] > media:
+        acima.append(p['nome'])
 
 print(lista)
 print('-=' * 30)
-print(f'   - O grupo tem {cont} pessoas.')
-print(f'   - A média de idade é de {media :.2f} anos.')
-print(f'   - As mulheres cadastradas foram: {fem}')
-print(f'   - Lista das pessoas que estão acima da média: {acima}')
+print(f'A) Ao todo temos {cont} pessoas cadastradas.')
+print(f'B) A média de idade é de {media :.2f} anos.')
+print(f'C) As mulheres cadastradas foram: ', end='')
+for p in lista:
+    if p['sexo'] == 'F':
+        print(f'{p["nome"]}, ', end='')
+print(f'D) Lista das pessoas que estão acima da média: ', end='')
+for p in lista:
+    if p['idade'] >= media:
+        print('      ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<< ENCERRADO >>')
