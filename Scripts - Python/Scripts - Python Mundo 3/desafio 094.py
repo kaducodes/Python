0e11ed95d10e9046827706e9e8b76449d5f2fadb
kaducodes <kadu.codes@gmail.com>
2025-03-08 11:41:03 -0300

    commit 08/03/2025 - 1
    exercício de dicionário

--- Scripts - Python/Scripts - Python Mundo 3/desafio 094.py
+++ Scripts - Python/Scripts - Python Mundo 3/desafio 094.py
@@ -13,3 +13,37 @@
 #- A média de idade é de x anos.
 #- As mulheres cadastradas foram: 
 #- Lista das pessoas que estão acima da média: 
+
+lista = []
+fem = []
+acima = []
+cont = 0
+idade = 0
+media = 0
+
+while True:
+    cont += 1
+    perfil = {}
+    perfil['nome'] = str(input("Nome: "))
+    perfil['sexo'] = str(input("Sexo [M/F]: ")).upper().strip()
+    if perfil['sexo'] == "F":
+        fem.append(perfil['nome'])
+    perfil['idade'] = int(input("Idade: "))
+    idade += perfil['idade']
+    lista.append(perfil.copy())
+
+    resp = str(input("Quer continuar? [S/N]: ")).upper().strip()
+    if resp == "N":
+        break
+
+media = idade / cont
+for pessoa in lista:
+    if pessoa['idade'] > media:
+        acima.append(pessoa['nome'])
+
+print(lista)
+print('-=' * 30)
+print(f'   - O grupo tem {cont} pessoas.')
+print(f'   - A média de idade é de {media :.2f} anos.')
+print(f'   - As mulheres cadastradas foram: {fem}')
+print(f'   - Lista das pessoas que estão acima da média: {acima}')
