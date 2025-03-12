fba10515f9a1a69d1165c6b65812c46d32dd2013
kaducodes <kadu.codes@gmail.com>
2025-03-12 14:51:52 -0300

    commit 12/03/2025 - 1
    resolução de exercícios com funções envolvendo docstrings e return

--- Scripts - Python/Scripts - Python Mundo 3/desafio 101.py
+++ Scripts - Python/Scripts - Python Mundo 3/desafio 101.py
@@ -1,3 +1,18 @@
-#Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem coto NEGADO, OPCIONAL o OBRIGATÓRIO nas eleições
+#Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL o OBRIGATÓRIO nas eleições
 
-    
+def voto(ano):   
+    from datetime import date 
+    ano_atual = date.today().year
+    idade = ano_atual - ano 
+    if idade < 16:
+        return f'Com {idade} anos: NÃO OBRIGATÓRIO.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: OPCIONAL.'
+    else:
+        return f'Com {idade} anos: OBRIGATÓRIO.'
+        
+
+#Programa principal
+print('-' * 40)
+a = int(input("Em que ano você nasceu? "))
+print(voto(a))
--- Scripts - Python/Scripts - Python Mundo 3/desafio 102.py
+++ Scripts - Python/Scripts - Python Mundo 3/desafio 102.py
@@ -1,4 +1,4 @@
-#Crie um programa que tenha uma função chamada fatorial() que receba 2 parâmetros: o primeiro que indique o número a calcular e o outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo ddo fatorial
+#Crie um programa que tenha uma função chamada fatorial() que receba 2 parâmetros: o primeiro que indique o número a calcular e o outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial
 
 #fatorial (3, show=False)
 #6
@@ -7,3 +7,25 @@
 
 #help(fatorial)
 #documenta a função
+
+def fatorial(num=1, show=False):
+    """
+    -> Calcula o fatorial de um número
+    : parâmetro num: O número a ser calculado
+    : parâmetro show: (opcional) Mostrar ou não a conta
+    : return: O valor do Fatorial de um número
+    """
+    f = 1
+    for c in range(num, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f
+
+
+print(fatorial(5, show=True))
+help(fatorial)
