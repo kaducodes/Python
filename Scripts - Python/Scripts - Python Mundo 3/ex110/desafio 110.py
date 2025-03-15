ce8df13d5c079f8aaaf28e6a508afa756fb21ad5
kaducodes <kadu.codes@gmail.com>
2025-03-14 21:25:50 -0300

    commit 14/03/2025 - 2
    complemento e resolução de exercícios de módulos e pacotes

--- Scripts - Python/Scripts - Python Mundo 3/ex110/desafio 110.py
+++ Scripts - Python/Scripts - Python Mundo 3/ex110/desafio 110.py
@@ -1,1 +1,13 @@
 #Adicione ao módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+
+from ex110 import moeda
+
+p = float(input('Digite o Preço: R$ '))
+moeda.resumo(p, 80, 35)
+#80% de aumento
+#35% de redução
+#-----------------------------
+#        RESUMO DO VALOR
+#-----------------------------
+#Preço analisado: R$
+#Dobro do preço:  R$
