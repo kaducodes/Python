461ebc7e446e322be9041118297b97008e724bc9
kaducodes <kadu.codes@gmail.com>
2025-03-19 11:55:15 -0300

    commit 19/03/2025 - 1
    resolução de exercícios de módulos e bibliotecas.

--- Scripts - Python/Scripts - Python Mundo 3/ex109/moeda.py
+++ Scripts - Python/Scripts - Python Mundo 3/ex109/moeda.py
@@ -1,26 +1,26 @@
-def metade(valor, moeda=False):    
-    if moeda:
+def metade(valor, formatado=False):    
+    if formatado:
         return f'R$ {valor :.2f}'
     else: 
         return valor / 2
 
 
-def dobro(valor, moeda=False):
-    if moeda:
+def dobro(valor, formatado=False):
+    if formatado:
         return f'R$ {valor :.2f}'
     else: 
         return valor * 2
 
 
-def aumentar(valor, porcentagem, moeda=False):
-    if moeda:
+def aumentar(valor, porcentagem, formatado=False):
+    if formatado:
         return f'R$ {valor :.2f}'
     else: 
         return valor * (1 + porcentagem / 100)
 
 
-def diminuir(valor, porcentagem, moeda=False):
-    if moeda:
+def diminuir(valor, porcentagem, formatado=False):
+    if formatado:
         return f'R$ {valor :.2f}'
     else: 
         return valor * (1 - porcentagem / 100)
