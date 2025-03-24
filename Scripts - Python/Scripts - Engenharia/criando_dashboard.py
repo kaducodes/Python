7535de08ea0a815878365774ebb870ef9bc861d4
kaducodes <kadu.codes@gmail.com>
2025-03-24 14:26:52 -0300

    commit 24/03/2025 - 1
    resolução e finalização do exercício de módulo e manipulação de arquivo em python. finalização do mundo 3

--- Scripts - Python/Scripts - Engenharia/criando_dashboard.py
+++ Scripts - Python/Scripts - Engenharia/criando_dashboard.py
@@ -1,5 +1,8 @@
-import streamlit as st
-import pandas as pd
-import plotly.express as px
+#import streamlit as st
+#import pandas as pd
+#import plotly.express as px
 
-df = pd.read_csv("custos_irlena.csv", sep=";" decimal=".")
+#df = pd.read_csv("custos_irlena.csv", sep=";" decimal=".")
+
+letras = ('J', 'X', 'M', 'O', 'A', 'K')
+print(letras[0], letras[-1])
--- Scripts - Python/Scripts - Python Mundo 3/desafio 115.py
+++ Scripts - Python/Scripts - Python Mundo 3/desafio 115.py
@@ -17,7 +17,10 @@ while True:
         #Opção de listar o conteúdo de um arquivo
         lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabeçalho('Saindo do sistema... Até logo!')
         break
